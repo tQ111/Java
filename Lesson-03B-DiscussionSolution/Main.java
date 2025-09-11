47837e2e19d325d56652fac44c66ac8984a701db
tQ111 <tondisx@gmail.com>
2025-09-11 13:33:29 +0000

    w

---
+++ Lesson-03B-DiscussionSolution/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+
+    System.out.println("\"Hello\"");
+
+    System.out.println("one \ntwo \nthree");
+    
+    System.out.println("\tErnest Porchetta");
+    
+  }
+
+  
+}
