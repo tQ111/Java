7ee66903cb3fde788c9975066f30153a923c01f7
tQ111 <tondisx@gmail.com>
2025-10-01 13:14:17 +0000

    w

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,49 @@
+class Main {
+
+	public static void main(String[] args) {
+    	
+	}
+
+	void print(String s){
+    System.out.println(s);
+  }
+
+  double FtoC(double tempF){
+    return ( tempF - 32 )*5.0/9.0;
+  }
+
+  double sphereVolume(double radius){
+    return ( 4/3.*Math.PI ) * Math.pow( radius, 3 );
+    
+  }
+  
+  double coneVolume(double radius,double height){
+	return 1.0/3.0 * ( height * Math.PI * Math.pow(radius, 2) );
+  
+  }
+
+  
+  void init(){
+
+    System.out.println( "Enter word" );
+    String name = Input.readString();
+    print( "Your word is " + name );
+
+    System.out.println( "Enter temperature (farenhiet)" );
+    double tempF = Input.readDouble();
+    print( tempF + "F =  " + FtoC(tempF) + "C" );
+
+    System.out.println("Enter the radius of the sphere ");
+    double radi= Input.readDouble();
+    print( "Volume of sphere= " + sphereVolume(radi) );
+
+    System.out.println("Enter radius");
+    radi= Input.readDouble();
+    System.out.println( "Enter height" );
+    double height = Input.readDouble();
+    print( "Volume of cone= " + coneVolume(radi,height) );
+
+  }
+}
+
+ 
