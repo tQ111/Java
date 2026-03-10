fc1ca6b2cca2a0b84b52ac40b622303b67b9e0ab
tQ111 <tondisx@gmail.com>
2026-03-10 13:32:29 +0000

    w

---
+++ Lesson-40-Activity/Lesson-40-Activity/Main.java
@@ -0,0 +1,36 @@
+
+import java.io.IOException;
+import java.sql.*;
+//For compiling on the shell on repl: Same on mac
+//javac -cp sqlite-jdbc-3.23.1.jar: Main.java
+
+//Use for windows
+//javac -cp sqlite-jdbc-3.23.1.jar; ServerExample.java
+class Main {
+
+ public static void main(String[] args)throws IOException{
+    (new Main()).init();
+  }
+
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init() {
+   
+
+    String queryResult = "";
+    String sql = "Select * From students limit 10;";
+
+    Database db = new Database("jdbc:sqlite:students.db");
+    queryResult = db.runSQL(sql, "table-auto");
+    print(queryResult);
+
+
+
+    
+
+
+    
+     
+  }    
+}
---
+++ Lesson-40-Classwork/Lesson-40-Classwork/Main.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.sql.*;
+//For compiling on the shell on repl: Same on mac
+//javac -cp sqlite-jdbc-3.23.1.jar: Main.java
+
+//Use for windows
+//javac -cp sqlite-jdbc-3.23.1.jar; ServerExample.java
+class Main {
+
+ public static void main(String[] args)throws IOException{
+    (new Main()).init();
+  }
+
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init() {
+     
+
+    String queryResult = "";
+    String sql = "Select * From cr101 where Room = '106' AND Period = '3';";
+
+    Database db = new Database("jdbc:sqlite:cr101.db");
+    queryResult = db.runSQL(sql, "table-auto");
+    print(queryResult);
+      
+      
+      
+  }    
+}
